5ccef0b619deb6d5da324348b0a6168d3c88585b
rrathi0705 <rrathi0705@gmail.com>
2018-04-14 10:58:04 +0530

    added further functionality to calculator like remove repeated decimal etc

--- script.js
+++ script.js
@@ -4,16 +4,25 @@ $(document).ready(function(){
   var len = 0;
   var curr="";
   var lastinput = "";
+  var flag = 0; // for counting the number of decimals have already there in the input
   $("button").on('click',function(){
       curr = $(this).attr("value");
         lastinput = input.charAt(len-1);
-      //console.log(curr);
+      // Clearing the input-output-box field
+      
       if(curr == 'ac'){
         input = '';
         len = 0;
         $("#input-output-box").val('0');
+        flag = 0;
       }
+      
+      // backspace one character in the input-output-box
+
       else if(curr == 'del'){
+        if(lastinput =='.'){
+        	flag = 0;
+        }
         if(len <= 1)
         {
           input = '';
@@ -25,26 +34,79 @@ $(document).ready(function(){
         len--;
         $("#input-output-box").val(input);
         }
-        //console.log(len);
       }
-      else if(curr == '*' || curr == '/' || curr == '+' || curr == '-'){
-        //console.log(curr);
+      /* 
+     	  If incoming charcater is multiply or divide 
+      	  1 If lastchar is number then just append to the input string
+      	  2 else if last character is operator then repalce the incoming char with the last char
+      */
+      else if(curr == '*' || curr == '/'){
         if(len!=0){
           if(lastinput =='*' || lastinput =='/' || lastinput =='+' || lastinput =='-'){
             input = input.slice(0,len-1);
             len--; 
           }
-          //console.log(curr);
           input+= curr;
           len++;
+          flag = 0;
           $("#input-output-box").val(input);
         }
       }
+      /*
+		If incoming char is plus or minus then
+		1 If lastchar is number then just append to input
+		2 else if lastchar is + or - then replace it with incoming symbol
+		3 else if lastchar is * or / then append incoming symbol to input as 2*-4 = -8
+      */
+      else if(curr =='+' || curr=='-'){
+      	if(len!=0){
+          if(lastinput =='+' || lastinput =='-'){
+            input = input.slice(0,len-1);
+            len--; 
+          }
+          input+= curr;
+          len++;
+          flag = 0;
+          $("#input-output-box").val(input);
+        }
+      }
+      /*
+		Equal button pressed then evaluate the string
+		and update the length and lastinputchar of the string
+      */
       else if(curr === '='){
-        $("#input-output-box").val(eval(input));
-        input = eval(input).toString();
+      	var ans = parseFloat(eval(input)).toFixed(3);
+        $("#input-output-box").val(ans);
+        input = ans.toString();
         len = input.length;
         lastinput = input.charAt(len-1);
+      }
+      /*
+		if decimal is pressed then if decimal is already present in the number then don't allow if
+		else allow the decimal insertion
+		2.3+4.5 allowed
+		2.3.4 disallowed
+      */
+      else if(curr == '.'){
+      	if(flag == 0){
+      		input +=curr;
+      		len++;
+      		flag = 1;	
+      	}
+      	$("#input-output-box").val(input);
+      }
+      /*
+		if 0 is pressed first then let the input string remain same 
+		else append 0 to the string
+      */
+      else if(curr =='0'){
+      	if(len != 0){
+      		input += curr;
+      		len++;
+      		$("#input-output-box").val(input);
+      	}
+      	else
+      	$("#input-output-box").val('0');
       }
       else{
         input +=curr; 
